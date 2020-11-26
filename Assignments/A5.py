d281f98fe17d235679f8342f045049589447ad43
Osama Khan <Osama6756@gmail.com>
2020-11-26 05:46:55 +0500

    Create A5.py

---
+++ Assignments/A5.py
@@ -0,0 +1,33 @@
+# Define two functions (one iterative and other recursive)
+# in Python named sub-it and sub-rec. Each of the functions
+# will take three arguments; old-value, new-value, and a
+# list. They should replace old-value with the new-value
+# from the given list.
+
+def sub_it(old, new, arr):
+    out = list()
+    for e in arr:
+        if (e == old):
+            out.append(new)
+        else:
+            out.append(e)
+    return out
+
+
+def sub_rec(old, new, arr):
+    out = []
+    if len(arr) != 1:
+        if arr[0] == old:
+            out.append(new)
+        else:
+            out.append(arr[0])
+        out.append(sub_rec(old, new, arr[1:]))
+        return out
+    else:
+        if arr[0] == old:
+            return new
+        return arr[0]
+
+
+print(sub_it(10, 5, [10, 8, 10, 12]))
+print(sub_rec(10, 5, [10, 8, 10, 12]))
